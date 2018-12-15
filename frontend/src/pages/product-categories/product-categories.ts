9f898848b60a5fc070386214f483c7b1e4538787
shengliang <ahliang51@hotmail.com>
2018-12-15 13:12:40 +0800

    edited product categories background image

--- frontend/src/pages/product-categories/product-categories.ts
+++ frontend/src/pages/product-categories/product-categories.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProductProvider } from '../../providers/product/product';
+import { ImageAttribute } from 'ionic-image-loader'
 
 /**
  * Generated class for the ProductCategoriesPage page.
@@ -17,10 +18,17 @@ import { ProductProvider } from '../../providers/product/product';
 export class ProductCategoriesPage {
 
   productCategories
+  imageAttributes: ImageAttribute[] = [];
+
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    public productProvider: ProductProvider) {
+    public productProvider: ProductProvider,
+  ) {
+    this.imageAttributes.push({
+      element: 'class',
+      value: 'product-categories'
+    })
   }
 
   ionViewDidLoad() {
